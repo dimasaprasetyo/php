011ab8e3070b3700aaf3b0dacd61e6dfe2096b44
Dimas Agung Prasetyo <dimasaung@gmail.com>
2022-04-26 14:16:29 +0700

    Percobaan Kedua

---
+++ php-2/looping.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Looping</title>
+    </head>
+    <body>
+        <h1>Berlatih Looping</h1>
+        <?php
+        echo "<h3>Soal No 1 Looping I Love PHP</h3>";
+        /*
+        Soal No 1
+        Looping I Love PHP
+        Lakukan Perulangan (boleh for/while/do while) sebanyak 20 iterasi. Looping terbagi menjadi dua: Looping yang pertama Ascending
+        (meningkat)
+        dan Looping yang ke dua menurun (Descending).*/
+        echo "<h3>Looping Pertama</h3>";
+        $bilangan = 2;
+        while($bilangan <= 20) {
+            echo "$bilangan - I Love PHP<br>";
+            $bilangan+=2;
+        }
+        echo "<h3>Looping Kedua</h3>";
+        $bilangan = 20;
+        while($bilangan > 0) {
+            echo "$bilangan - I Love PHP<br>";
+            $bilangan-=2;
+        }
+        ?>
+        <hr>
+        <?php
+        echo "<h3>Soal No 2 Looping Array Module</h3>";
+        /*
+        Soal No 2
+        Looping Array Module
+        Carilah sisa bagi dengan angka 5 dari setiap angka pada array berikut.
+        Tampung ke dalam array baru bernama $rest
+        */
+        $numbers = [18, 45, 29, 61, 47, 34];
+        echo "array numbers: ";
+        print_r($numbers);
+        // Lakukan Looping di sini
+        for ($i=0; $i < count($numbers);  $i++) {
+            $rest[$i] = $numbers[$i] % 5;
+        };
+        echo "<br>";
+        echo "Array sisa baginya adalah:  ";
+        print_r($rest);
+        echo "<br>";    
+        ?>
+        <hr>
+        <?php     
+        echo "<h3> Soal No 3 Looping Asociative Array </h3>";
+        /*
+        Soal No 3
+        Loop Associative Array
+        Terdapat data items dalam bentuk array dimensi. Buatlah data tersebut ke dalam bentuk Array Asosiatif.
+        Setiap item memiliki key yaitu : id, name, price, description, source.
+        
+        Output:
+        Array ( [id] => 001 [name] => Keyboard Logitek [price] => 60000 [description] => Keyboard yang mantap untuk kantoran [source]
+        => logitek.jpeg )
+        Array ( [id] => 002 [name] => Keyboard MSI [price] => 300000 [description] => Keyboard gaming MSI mekanik [source] => msi.jpeg
+        )
+        Array ( [id] => 003 [name] => Mouse Genius [price] => 50000 [description] => Mouse Genius biar lebih pinter [source] =>
+        genius.jpeg )
+        Array ( [id] => 004 [name] => Mouse Jerry [price] => 30000 [description] => Mouse yang disukai kucing [source] => jerry.jpeg
+        )
+        */
+        $items = [
+        ['001', 'Keyboard Logitek', 60000, 'Keyboard yang mantap untuk kantoran', 'logitek.jpeg'],
+        ['002', 'Keyboard MSI', 300000, 'Keyboard gaming MSI mekanik', 'msi.jpeg'],
+        ['003', 'Mouse Genius', 50000, 'Mouse Genius biar lebih pinter', 'genius.jpeg'],
+        ['004', 'Mouse Jerry', 30000, 'Mouse yang disukai kucing', 'jerry.jpeg']
+        ];
+        // Output:
+        // for ($i=0; $i < count($items);  $i++) {
+        //     $tampung[$i] = $items[$i];
+        // }
+        // print_r ($tampung);
+
+        echo $items[0][0].' '.$items[0][1].' '.$items[0][2].' '.$items[0][3].' '.$items[0][4];
+// $siswa1 = [ "nama" => "abduh",
+// "kelas" => "laravel",
+// "nilai" => 70
+// ];
+// print_r($siswa1);
+// menambahkan key value baru ke array $siswa1
+// $siswa1["email"] = "abduh@mail.com";
+// print_r($siswa1);
+//         ?>
+            <?php
+            echo "<h3>Soal No 4 Asterix </h3>";
+            /*
+            Soal No 4
+            Asterix 5x5
+            Tampilkan dengan looping dan echo agar menghasilkan kumpulan bintang dengan pola seperti berikut:
+            Output:
+            *
+            * *
+            * * *
+            * * * *
+            * * * * *
+            */
+            $star=5;
+            for($a=$star;$a>0;$a--){
+                for($b=$star;$b>=$a;$b--){
+                    echo "*";
+                }
+                echo "<br>";
+            }            
+            
+            ?>
+
+
+
+
+
+
+
+
+
+        
+        
+        
+
+</body>
+</html>
